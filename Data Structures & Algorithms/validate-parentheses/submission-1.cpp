31d1e602b0fca2a8ae44504d8c65b98ab948e2d5
SuhaasNachannagari <143771172+SuhaasNachannagari@users.noreply.github.com>
2026-04-02 17:40:00 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,29 @@
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parentheses;
+        unordered_map<char, char> mapping = 
+            {
+                {')', '('},
+                {'}', '{'},
+                {']', '['}
+            };
+
+        for (char c : s) {
+            if (mapping.count(c) > 0) {
+                if (!parentheses.empty() && mapping[c] == parentheses.top()) {
+                    parentheses.pop();
+                } else {
+                    return false;
+                }
+                
+            } else {
+                parentheses.push(c);
+            }
+        }
+
+        return parentheses.empty();
+    }
+};
